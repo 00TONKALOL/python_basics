37d077d38beb199dc84339c5b70c25a5d85f82df
emobilis <ernestmaina@gmail.com>
2025-03-18 10:46:00 +0300

    Math files

--- number_functions.py
+++ number_functions.py
@@ -4,3 +4,18 @@ print(math.sqrt(x))
 print(math.pi)
 print(math.floor(56.88))
 print(math.ceil(56.88))
+
+radius=54
+height=145
+
+volume=22/7 *radius*radius*height
+volume=round(volume,1)
+print(volume)
+
+sa= 2*22/7*radius*(radius+height)
+sa=round(sa,1)
+print(sa)
+
+result = 10 + 20 / 5 * 6 - 3 * (5+2)
+print(result)
+
