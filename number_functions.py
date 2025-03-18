16433438bbe04d4d8027ca4ee16d33402cf9b352
emobilis <ernestmaina@gmail.com>
2025-03-18 10:22:29 +0300

    Math files

---
+++ number_functions.py
@@ -0,0 +1,6 @@
+import math
+x = 4567896
+print(math.sqrt(x))
+print(math.pi)
+print(math.floor(56.88))
+print(math.ceil(56.88))
---
+++ strings.py
@@ -0,0 +1,14 @@
+messages="hello,how are you doing?"
+print(messages)
+#functions
+print(messages.capitalize())
+print(messages.upper())
+print(messages.lower())
+print(messages.islower())
+print(messages.isalpha())
+print(messages.count("d"))
+print(messages.isnumeric())
+print(messages.title())
+print(messages.endswith("d"))
+print(messages.replace("doing", "feeling"))
+
